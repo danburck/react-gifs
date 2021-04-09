55ec655b7d3cb64c3362f1840d267868e2f041fc
danburck <daniel.m.burckhardt@gmail.com>
2021-04-09 18:38:23 +0200

    added next TODO: add giflist

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 
 import Gif from './gif';
+import GifList from './giflist';
+
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
+
   render() {
+    // TODO: fetch gifs from giphy api
+    const gifs = [];
+
+
     return (
       <div>
         <div className="left-scene">
-          <Gif/>
+          <Gif />
         </div>
         <div className="right-scene">
+          <GifList gifs={gifs} />
         </div>
       </div>
     );
